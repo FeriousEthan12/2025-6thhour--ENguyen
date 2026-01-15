4da00be3776e6d3df98f6c38e7d3a828e482fd08
Ethan Nguyen <en128114@student.wpsok.org>
2026-01-15 13:29:22 -0600

    HW16 done

--- The Orb.py
+++ The Orb.py
@@ -3,8 +3,8 @@ import random
 question = input("Do you want to ask the orb (y/n): ")
 while True:
     if question == "y":
-        responses = ["Sure", "That is seared into fate", "HECK YEA", "Do that and times it by 100", "Sure?", "That seems ok", "nah", "HECK NO", "Don't ask me that again", "...", "What? I wasn't listening ",
-                     "nooooooooo"]
+        responses = ["Sure", "That is burned into fate", "HECK YEA", "Yes and times it by 100", "Sure?",
+                     "That seems ok", "nah", "HECK NO", "Don't ask me that again", "...", "What? I wasn't listening ", "nooooooooo"]
         print(random.choice(responses))
         question = input("Do you want to ask the orb (y/n): ")
     elif question == "n":
