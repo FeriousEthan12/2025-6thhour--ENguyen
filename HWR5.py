63b15ea8882dec78f2049e76dee70c3b448bd566
Ethan Nguyen <en128114@student.wpsok.org>
2026-04-08 13:43:47 -0500

    Finished HWR5

---
+++ HWR5.py
@@ -0,0 +1,16 @@
+#Name: Ethan Nguyen
+#Class: 6th Hour
+#Assignment: HW-R5
+
+#1. Fix the "class" comment at the top to "6th Hour"
+
+#2. Create a list of the names of all the students in the classroom.
+students = ["Connor", "Devon", "Alaya", "Shane", "Ally", "Tristan", "Ethan", "Greg"]
+#3. Create a for loop that prints the names of every student in the list.
+#for i in students:
+#    print(i)
+#4. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it.
+for p in students:
+    if 'e' in p or 'E' in p :
+        print(p)
